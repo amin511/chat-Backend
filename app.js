3e8c3688276bc741347a6961c38b417a5f8b1eb5
amin511 <amindarmouni3@gmail.com>
2023-08-08 20:52:49 +0100

    first version

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ start();
 
 const io = socketIO(server, {
     cors: {
-        origins: 'https://chat-website-frontend-pwt9.vercel.app', // Replace with your React app's URL
+        origins: 'https://chat-website-frontend-ndsfhrywc-amin511.vercel.app', // Replace with your React app's URL
         methods: ['GET', 'POST'],
     },
 });
