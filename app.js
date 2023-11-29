3feb08c1d877dc0e8e48ca18f1c19e9894a86644
amin511 <amindarmouni3@gmail.com>
2023-11-29 15:01:40 +0100

    add cloudinary for images

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 //** 
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors())
 app.use(helmet());
 app.use(xss());
@@ -78,7 +78,7 @@ let usersOnline = [];
 
 const io = socketIO(server, {
     cors: {
-        origin: 'https://chat-amin.netlify.app', // Replace with your React app's URL
+        origin: 'http://localhost:5173', // Replace with your React app's URL
         methods: ['GET', 'POST'],
     },
 });
